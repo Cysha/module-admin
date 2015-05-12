63b23bab6e3fc6ee3476e02d4aa86616a2f5c68e
xLink <xLink@cybershade.org>
2015-05-12 10:32:35 +0100

    added datatable package & admin menu setup

---
+++ Config/admin.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+
+    'acp_menu' => [
+        'System' => [
+            'admin.config.index' => '<i class="fa fa-fw fa-wrench"></i> Configuration',
+        ],
+    ],
+
+    'config_menu' => [
+        'admin.config.index' => '<i class="fa fa-fw fa-wrench"></i> Site Config',
+        'admin.theme.index'  => '<i class="fa fa-fw fa-wrench"></i> Theme Manager',
+    ],
+];
