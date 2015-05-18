186473b3f4b4ffae4a2c0eaa6580cd508f44b154
xLink <xLink@cybershade.org>
2015-05-18 01:48:56 +0100

    removed some old stuff from start.php, made all routes use  instead of facade

--- Http/routes-backend.php
+++ Http/routes-backend.php
@@ -1,35 +1,37 @@
 <?php
 
+use Illuminate\Routing\Router;
+
 // URI: /{backend}/config
-Route::group(['prefix' => 'config', 'namespace' => 'Config'], function () {
+$router->group(['prefix' => 'config', 'namespace' => 'Config'], function (Router $router) {
 
-    Route::get('website', ['as' => 'admin.config.website', 'uses' => 'WebsiteController@getIndex']);
-    Route::get('theme', ['as' => 'admin.config.theme', 'uses' => 'ThemeController@getIndex']);
-    Route::get('services', ['as' => 'admin.config.services', 'uses' => 'ServicesController@getIndex']);
-    Route::get('routes', ['as' => 'admin.config.routes', 'uses' => 'RoutesController@getIndex']);
-    Route::get('cache', ['as' => 'admin.config.cache', 'uses' => 'CacheController@getIndex']);
-    Route::get('debug', ['as' => 'admin.config.debug', 'uses' => 'DebugController@getIndex']);
+    $router->get('website', ['as' => 'admin.config.website', 'uses' => 'WebsiteController@getIndex']);
+    $router->get('theme', ['as' => 'admin.config.theme', 'uses' => 'ThemeController@getIndex']);
+    $router->get('services', ['as' => 'admin.config.services', 'uses' => 'ServicesController@getIndex']);
+    $router->get('routes', ['as' => 'admin.config.routes', 'uses' => 'RoutesController@getIndex']);
+    $router->get('cache', ['as' => 'admin.config.cache', 'uses' => 'CacheController@getIndex']);
+    $router->get('debug', ['as' => 'admin.config.debug', 'uses' => 'DebugController@getIndex']);
 
-    Route::post('save', ['as' => 'admin.config.store', 'uses' => 'WebsiteController@postStoreConfig']);
-    Route::get('/', function () {
+    $router->post('save', ['as' => 'admin.config.store', 'uses' => 'WebsiteController@postStoreConfig']);
+    $router->get('/', function () {
         return Redirect::route('pxcms.admin.index');
     });
 });
 
 // URI: /{backend}/dashboard
-Route::group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function () {
-    Route::post('saveGrid', ['as' => 'admin.dashboard.savegrid', 'uses' => 'DashboardController@saveGrid']);
-    Route::post('loadWidget', ['as' => 'admin.dashboard.widget', 'uses' => 'DashboardController@loadWidget']);
+$router->group(['prefix' => 'dashboard', 'namespace' => 'Dashboard'], function (Router $router) {
+    $router->post('saveGrid', ['as' => 'admin.dashboard.savegrid', 'uses' => 'DashboardController@saveGrid']);
+    $router->post('loadWidget', ['as' => 'admin.dashboard.widget', 'uses' => 'DashboardController@loadWidget']);
 
-    Route::get('/', ['as' => 'pxcms.admin.index', 'uses' => 'DashboardController@getIndex']);
+    $router->get('/', ['as' => 'pxcms.admin.index', 'uses' => 'DashboardController@getIndex']);
 });
 
 // the admin authentication routes
-Route::get('login', ['as' => 'pxcms.admin.login', 'uses' => 'AuthController@getLogin']);
-Route::post('login', 'AuthController@postLogin');
-Route::get('logout', ['as' => 'pxcms.admin.logout', 'uses' => 'AuthController@getLogout']);
+$router->get('login', ['as' => 'pxcms.admin.login', 'uses' => 'AuthController@getLogin']);
+$router->post('login', 'AuthController@postLogin');
+$router->get('logout', ['as' => 'pxcms.admin.logout', 'uses' => 'AuthController@getLogout']);
 
 // i said, the dashboard!
-Route::get('/', function () {
+$router->get('/', function () {
     return Redirect::route('pxcms.admin.index');
 });
--- start.php
+++ start.php
@@ -1,8 +1,1 @@
 <?php
-
-$module = last(explode(DIRECTORY_SEPARATOR, __DIR__));
-$namespace = sprintf('\Cms\Modules\%s\Http\Controllers', $module);
-
-require __DIR__ . '/Http/routes-frontend.php';
-require __DIR__ . '/Http/routes-backend.php';
-require __DIR__ . '/Http/routes-api.php';
