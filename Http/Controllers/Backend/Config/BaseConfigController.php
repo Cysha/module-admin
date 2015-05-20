b854be220868d740d01655640bb00947f5dc3b6d
xLink <xLink@cybershade.org>
2015-05-20 01:37:50 +0100

    fixed the config storing setup

--- Http/Controllers/Backend/Config/BaseConfigController.php
+++ Http/Controllers/Backend/Config/BaseConfigController.php
@@ -19,7 +19,7 @@ class BaseConfigController extends BaseAdminController
      */
     public function postStoreConfig()
     {
-        $settings = Input::except('_token');
+        $settings = array_except($this->getRealInput(), '_token');
 
         $failed = [];
         foreach ($settings as $setting => $value) {
@@ -36,6 +36,34 @@ class BaseConfigController extends BaseAdminController
         }
 
         return redirect()->back()->withInfo('Config Saved');
+    }
+
+    /**
+     * PHP replaces .'s with _'s in most variables and many others,
+     * this function will get the real var names
+     *
+     * @see  http://php.net/manual/en/language.variables.external.php
+     */
+    private function getRealInput()
+    {
+        $input = file_get_contents("php://input");
+        $return = [];
+        if (empty($input)) {
+            return $return;
+        }
+
+        $input = explode('&', $input);
+        if (!is_array($input)) {
+            return $return;
+        }
+
+        foreach ($input as $pair) {
+            list($name, $value) = explode('=', $pair);
+
+            $return[urldecode($name)] = urldecode($value);
+        }
+
+        return $return;
     }
 
 }
