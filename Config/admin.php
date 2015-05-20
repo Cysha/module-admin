df8064bd43c731a15f5a1b959b1dfa1ba2822625
Dan Aldridge <xLink@cybershade.org>
2015-05-20 12:12:51 +0100

    added an order the menu items

--- Config/admin.php
+++ Config/admin.php
@@ -8,18 +8,21 @@ return [
                 'route'      => 'admin.config.website',
                 'text'       => 'Configuration',
                 'icon'       => 'fa-wrench',
+                'order'      => 1,
                 'permission' => 'manage@admin_config'
             ],
             [
                 'route'      => 'admin.config.theme',
                 'text'       => 'Theme Manager',
                 'icon'       => 'fa-image',
+                'order'      => 2,
                 'permission' => 'theme@admin_config'
             ],
             //[
             //    'route'      => 'admin.module.manager',
             //    'text'       => 'Module Manager',
             //    'icon'       => 'fa-puzzle-piece',
+            //    'order'      => 3,
             //    'permission' => 'manage@admin_modules'
             //],
         ],
@@ -30,30 +33,35 @@ return [
             'route'      => 'admin.config.website',
             'text'       => 'Website Configuration',
             'icon'       => 'fa-wrench',
+            'order'      => 1,
             'permission' => 'website@admin_config'
         ],
         [
             'route'      => 'admin.config.services',
             'text'       => 'API Keys',
             'icon'       => 'fa-key',
+            'order'      => 2,
             'permission' => 'services@admin_config'
         ],
         [
             'route'      => 'admin.config.routes',
             'text'       => 'Base Routes',
             'icon'       => 'fa-sitemap',
+            'order'      => 3,
             'permission' => 'routes@admin_config'
         ],
         [
             'route'      => 'admin.config.cache',
             'text'       => 'Cache',
             'icon'       => 'fa-recycle',
+            'order'      => 4,
             'permission' => 'cache@admin_config'
         ],
         [
             'route'      => 'admin.config.debug',
             'text'       => 'Debug / Maintenance',
             'icon'       => 'fa-cogs',
+            'order'      => 5,
             'permission' => 'debug@admin_config'
         ],
     ],
