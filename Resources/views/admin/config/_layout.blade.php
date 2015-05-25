f24989041f62d206066af427cfaa07de669ea201
xLink <xLink@cybershade.org>
2015-05-25 20:15:55 +0100

    added panel title class

--- Resources/views/admin/config/_layout.blade.php
+++ Resources/views/admin/config/_layout.blade.php
@@ -1,7 +1,9 @@
 <div class="row">
     <div class="col-md-{{ $col_one or '3'}}">
         <div class="panel panel-default">
-            <div class="panel-heading">Config</div>
+            <div class="panel-heading">
+                <div class="panel-title">Config</div>
+            </div>
             <div class="panel-body">@menu('backend_config_menu')</div>
         </div>
     </div>
