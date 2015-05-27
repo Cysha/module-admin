0c7845d594bd5c3427b856750c42a130f90a5e4a
Dan Aldridge <xLink@cybershade.org>
2015-05-27 13:41:26 +0100

    updated controllers to match core module update

--- Http/Controllers/Backend/BaseAdminController.php
+++ Http/Controllers/Backend/BaseAdminController.php
@@ -1,7 +1,7 @@
 <?php namespace Cms\Modules\Admin\Http\Controllers\Backend;
 
-use Cms\Modules\Core\Http\Controllers\BaseAdminController as BAC;
+use Cms\Modules\Core\Http\Controllers\BaseBackendController;
 
-class BaseAdminController extends BAC
+class BaseAdminController extends BaseBackendController
 {
 }
