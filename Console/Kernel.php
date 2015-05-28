b26a123aedc8b0d572640b56d1889c220d47f8d4
Dan Aldridge <xLink@cybershade.org>
2015-05-28 14:49:10 +0100

    added default structure for the scheduled commands

---
+++ Console/Kernel.php
@@ -0,0 +1,19 @@
+<?php namespace Cms\Modules\Admin\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
+     */
+    protected function schedule(Schedule $schedule)
+    {
+
+    }
+
+}
