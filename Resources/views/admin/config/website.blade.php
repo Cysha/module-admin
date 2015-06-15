b9f8f4dd00be90fc0779fa499a9a9052dcbfc1ae
xLink <xLink@cybershade.org>
2015-06-15 01:20:56 +0100

    moved the timezone setting

--- Resources/views/admin/config/website.blade.php
+++ Resources/views/admin/config/website.blade.php
@@ -8,7 +8,7 @@
         </div>
         <div class="panel-body">
             {!! Form::Config('cms.core.app.site-name')->label('Site Name') !!}
-            {!! Form::Config('app.timezone', 'select')->options($timezones)->label('Timezone') !!}
+            {!! Form::Config('cms.core.app.timezone', 'select')->options($timezones)->label('Timezone') !!}
             {!! Form::Config('cms.core.app.pxcms-index', 'select')->options($indexRoutes)->label('Set Homepage') !!}
             {!! Form::Config('cms.core.app.force-secure', 'radio')->radios(['Yes' => ['value' => 'true'], 'No' => ['value' => 'false']])->label('Force HTTPS?')->inline() !!}
             {!! Form::Config('cms.core.app.minify-html', 'radio')->radios(['Yes' => ['value' => 'true'], 'No' => ['value' => 'false']])->label('Minify HTML?')->inline() !!}
