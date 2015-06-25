f5da10465bd2a6058694d4ff3f1379407d6fa313
Dan Aldridge <xLink@cybershade.org>
2015-06-25 13:49:46 +0100

    whitespace fix

--- Config/permissions.php
+++ Config/permissions.php
@@ -2,27 +2,27 @@
 
 return [
     'admin_config' => [
-        'access'           => 'Have access to the admin panel',
-        'manage'           => 'Manage the admin config',
-        'website'          => 'Manage the website',
-        'theme'            => 'Change the theme',
-        'services'         => 'Manage site-wide services',
-        'cache'            => 'Manage the Cache',
-        'debug'            => 'Enable &amp; Disable debugging',
-        'routes'           => 'Clear and Cache routes',
+        'access' => 'Have access to the admin panel',
+        'manage' => 'Manage the admin config',
+        'website' => 'Manage the website',
+        'theme' => 'Change the theme',
+        'services' => 'Manage site-wide services',
+        'cache' => 'Manage the Cache',
+        'debug' => 'Enable &amp; Disable debugging',
+        'routes' => 'Clear and Cache routes',
     ],
 
     'admin_modules' => [
         // backend manager
-        'manage'           => 'Manage modules',
-        'manage.create'    => 'Create a module?',
-        'manage.read'      => 'Read a module\'s configuration',
-        'manage.update'    => 'Update a module\'s configuration',
-        'manage.delete'    => 'Remove a module from the site',
+        'manage' => 'Manage modules',
+        'manage.create' => 'Create a module?',
+        'manage.read' => 'Read a module\'s configuration',
+        'manage.update' => 'Update a module\'s configuration',
+        'manage.delete' => 'Remove a module from the site',
 
-        'module.install'   => 'Install a new module',
+        'module.install' => 'Install a new module',
         'module.uninstall' => 'Uninstall a module',
-        'module.toggle'    => 'Toggle a module\'s enabled state',
+        'module.toggle' => 'Toggle a module\'s enabled state',
     ],
 
 ];
