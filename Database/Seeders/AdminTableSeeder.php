5e6a1ff3802f3ebc42c9c3c5ee5f73ef794d0fc1
xLink <xLink@cybershade.org>
2015-11-18 15:44:41 +0000

    renamed the main seeder file

--- Database/Seeders/AdminTableSeeder.php
+++ Database/Seeders/AdminTableSeeder.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
 
-class AdminDatabaseSeeder extends Seeder
+class AdminTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
